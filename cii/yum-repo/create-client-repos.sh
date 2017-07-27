2baaa2bb04bd63044f956dcb0a7d76fc1a44d184
gdbc <graemedbc@gmail.com>
2017-07-27 12:42:38 +0100

    added create client repo script

---
+++ cii/yum-repo/create-client-repos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+REPO_SERVER="ci-pulp.example.com"
+REPOPATH="/etc/yum.repos.d/"
+REPO_URL="pulp/repos"
+REPOIDS="base extras updates puppetlabs-deps puppetlabs-products epel foreman foreman-plugins jenkins pulp-2-stable"
+ENV="7_master"
+
+# Remove existing yum repositories
+rm -rf /etc/yum.repos.d/*
+
+for i in $REPOIDS;do
+
+echo "
+[$i]
+name=$i Repository
+baseurl=http://$REPO_SERVER/$REPO_URL/"$i"-"$ENV"
+gpgcheck=0
+enabled=1
+" >>                    $REPOPATH/$i.repo
+
+done
